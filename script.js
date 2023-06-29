523d91ef9a435201cb63c4d657d68e9f77243142
Sillychunmaru <123471290+Sillychunmaru@users.noreply.github.com>
2023-06-29 11:37:35 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: James Viglino
+    Date: 6/7/2023
+
+    Filename: script.js
+*/
+
+//define variables for home and work addresses
+
+
+
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+
+let workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
